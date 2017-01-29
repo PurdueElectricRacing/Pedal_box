7c7305741310e14957ce32dc2c1b731850c0c397
kstrubel <kstrubel@purdue.edu>
2017-01-29 15:13:29 -0500

    Updated CAN header files

---
+++ UserTasks.h
@@ -0,0 +1,14 @@
+/*
+ * UserTasks.h
+ *
+ *  Created on: Jan 20, 2017
+ *      Author: Kai Strubel
+ */
+
+#ifndef USERTASKS_H_
+#define USERTASKS_H_
+
+void taskBlink_LED(int *ledID);
+//void taskBlink_LED(void *pvParams);
+
+#endif /* USERTASKS_H_ */
